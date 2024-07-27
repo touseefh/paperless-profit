e53544cd582ddb94710f2856c9dd6046ce780609
Touseef Hanif <123441664+TouseefH@users.noreply.github.com>
2024-07-27 12:35:48 +0500

    Update Customers.cshtml.cs

--- Pages/Customers/Customers.cshtml.cs
+++ Pages/Customers/Customers.cshtml.cs
@@ -9,7 +9,7 @@ namespace Bookshop.Pages.Customers
 {
     public class CustomersModel : PageModel
     {
-        private readonly string _connectionString = "Server=DESKTOP-S9EO4OF\\SQLEXPRESS;Database=Touseef;Trusted_Connection=True;TrustServerCertificate=True";
+        private readonly string _connectionString = "";
 
         public List<Customer> Customers { get; set; } = new List<Customer>();
 
