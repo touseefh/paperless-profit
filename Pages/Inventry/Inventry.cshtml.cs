51f81c2c6b7375d976baf357384e280e544b95d6
Touseef Hanif <123441664+TouseefH@users.noreply.github.com>
2024-07-27 12:36:26 +0500

    Update Inventry.cshtml.cs

--- Pages/Inventry/Inventry.cshtml.cs
+++ Pages/Inventry/Inventry.cshtml.cs
@@ -19,7 +19,7 @@ namespace Bookshop.Pages.Inventory
         {
             try
             {
-                string connectionString = "Server=DESKTOP-S9EO4OF\\SQLEXPRESS;Database=Touseef;Trusted_Connection=True;TrustServerCertificate=True";
+                string connectionString = "";
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     connection.Open();
